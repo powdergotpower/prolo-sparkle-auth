7345c29ead7c414a583a5c50b5cb280f9037d8ec
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-09-26 05:22:53 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.b781372df7a24ebb852cd3958c82df31',
+  appId: 'com.prolo.sparkleauth',
   appName: 'prolo-sparkle-auth',
   webDir: 'dist',
-  server: {
-    url: 'https://b781372d-f7a2-4ebb-852c-d3958c82df31.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   bundledWebRuntime: false
 };
 
